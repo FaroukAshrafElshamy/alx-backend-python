9dfc8ae9df9949664739178dd2397a0f6ddaf975
FaroukAshrafElshamy <faroukashrafelshamy321@gmail.com>
2024-08-05 18:01:32 +0300

    The basics of async

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""Libraries"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """async function"""
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
